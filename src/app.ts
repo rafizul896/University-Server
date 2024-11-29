9d13d192a8a6b7d457402eaab734081f81cad45e
rafizul896 <rafizulislam896@gmail.com>
2024-11-29 07:26:06 +0600

    add: notFound route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { StudentRouters } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
 const app: Application = express();
 
 // parsers
@@ -18,5 +19,6 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
+app.use(notFound)
 
 export default app;
