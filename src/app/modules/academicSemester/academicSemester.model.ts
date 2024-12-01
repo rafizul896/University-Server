58ddb1370edf69e1c99242f265616df20dc5e915
rafizul896 <rafizulislam896@gmail.com>
2024-12-01 06:58:53 +0600

    add: academicSemester interface and model added

---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,60 @@
+import { model, Schema } from 'mongoose';
+import { TAcademicSemester } from './academicSemester.interface';
+
+const academicSemesterSchema = new Schema<TAcademicSemester>({
+  name: {
+    type: String,
+    enum: ['Autumn', 'Summar', 'Fall'],
+    required: true,
+  },
+  code: {
+    type: String,
+    enum: ['01', '02', '03'],
+    required: true,
+  },
+  year: {
+    type: Date,
+    required: true,
+  },
+  startMonth: {
+    type: String,
+    enum: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
+    required: true,
+  },
+  endMonth: {
+    type: String,
+    enum: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
+    required: true,
+  },
+});
+
+export const academicSemester = model<TAcademicSemester>(
+  'academicSemester',
+  academicSemesterSchema,
+);
