cbd597a83365e2f87e9360b5b721b973c4dcbeae
rafizul896 <rafizulislam896@gmail.com>
2024-12-04 14:41:32 +0600

    add: academic faculty validation using zod

---
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+const academicFacultyValidationSchema = z.object({
+  body: z.object({
+    name: z
+      .string({
+        required_error: "Name is required",
+        invalid_type_error: "Name must be a string",
+      })
+      .min(1, "Name cannot be empty")
+      .max(255, "Name cannot exceed 255 characters"),
+  }),
+});
+
+export const AcademicFacultyValidations = {
+  academicFacultyValidationSchema,
+};
