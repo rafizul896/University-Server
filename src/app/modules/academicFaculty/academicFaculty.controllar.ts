e3026182d9f275d8897070f4aaa7563a8df82f2c
rafizul896 <rafizulislam896@gmail.com>
2024-12-05 20:09:34 +0600

    add: createAcademicFaculty controllar

---
+++ src/app/modules/academicFaculty/academicFaculty.controllar.ts
@@ -0,0 +1,21 @@
+import catchAsync from '../../utils/catchAsynce';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
+import { AcademicFacultyServices } from './academicFaculty.service';
+
+const createAcademicFaculty = catchAsync(async (req, res) => {
+  const result = await AcademicFacultyServices.createAcademicFacultyIntoDB(
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty is created succesfully',
+    data: result,
+  });
+});
+
+export const AcademicFacultyControllars = {
+  createAcademicFaculty,
+};
