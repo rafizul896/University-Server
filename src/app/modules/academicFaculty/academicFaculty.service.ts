bfe06dedbc60a28fd39d9d76001f4a87cbc53ace
rafizul896 <rafizulislam896@gmail.com>
2024-12-05 19:57:14 +0600

    add: get academicFaculty services

--- src/app/modules/academicFaculty/academicFaculty.service.ts
+++ src/app/modules/academicFaculty/academicFaculty.service.ts
@@ -2,10 +2,22 @@ import { TAcademicFaculty } from './academicFaculty.interface';
 import { AcademicFaculty } from './academicFaculty.model';
 
 const createAcademicFacultyIntoDB = async (payload: TAcademicFaculty) => {
-    const result = await AcademicFaculty.create(payload);
-    return result;
+  const result = await AcademicFaculty.create(payload);
+  return result;
+};
+
+const getAcademicFacultiesFromDB = async () => {
+  const result = await AcademicFaculty.find();
+  return result;
+};
+
+const getSingleAcademicFacultyFromDB = async (id: string) => {
+  const result = await AcademicFaculty.findById(id);
+  return result;
 };
 
 export const AcademicFacultyServices = {
   createAcademicFacultyIntoDB,
+  getAcademicFacultiesFromDB,
+  getSingleAcademicFacultyFromDB,
 };
