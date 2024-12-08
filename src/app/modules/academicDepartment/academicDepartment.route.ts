26ce7c887936d669b72647d06583777f96a2f437
rafizul896 <rafizulislam896@gmail.com>
2024-12-08 10:29:55 +0600

    add: academic department routes

---
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { AcademicDepartmentControllers } from './academicDepartment.controllar';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicDepartmentValidations } from './academicDepartment.validation';
+
+const router = Router();
+
+router.post(
+  '/create-academic-department',
+  validateRequest(
+    AcademicDepartmentValidations.createAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.createAcademicDepartment,
+);
+
+router.get('/', AcademicDepartmentControllers.getAllAcademicDepartments);
+
+router.get(
+  '/:departmentId',
+  AcademicDepartmentControllers.getSingleAcademicDepartment,
+);
+
+router.patch(
+  '/:departmentId',
+  validateRequest(
+    AcademicDepartmentValidations.updateAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.updateAcademicDepartment,
+);
+
+export const AcademicDepartmentRoutes = router;
