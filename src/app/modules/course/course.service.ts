4334498dc5a5cda1258e598f13647d8b7053875c
rafizul896 <rafizulislam896@gmail.com>
2024-12-22 12:57:03 +0600

    add: course services

---
+++ src/app/modules/course/course.service.ts
@@ -0,0 +1,41 @@
+import { TCourse } from './course.interface';
+import { Course } from './course.model';
+
+const createCourseIntoDB = async (payload: TCourse) => {
+  const result = await Course.create(payload);
+  return result;
+};
+
+const getAllCoursesFromDB = async () => {
+  const result = await Course.find();
+  return result;
+};
+
+const getSingleCourseFromDB = async (id: string) => {
+  const result = await Course.findById(id);
+  return result;
+};
+
+const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
+  const result = await Course.findByIdAndUpdate(id, payload);
+  return result;
+};
+
+const deleteCourseFromDB = async (id: string) => {
+  const result = await Course.findByIdAndUpdate(
+    id,
+    {
+      isdeleted: true,
+    },
+    { new: true },
+  );
+  return result;
+};
+
+export const courseServices = {
+  createCourseIntoDB,
+  getAllCoursesFromDB,
+  getSingleCourseFromDB,
+  updateCourseIntoDB,
+  deleteCourseFromDB,
+};
