e7e86fb05f03c6682cf1fee54719e2544682665e
rafizul896 <rafizulislam896@gmail.com>
2024-12-27 11:40:43 +0600

    add: Verify token using auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,6 +1,8 @@
+import config from '../config';
 import AppError from '../errors/AppError';
 import catchAsync from '../utils/catchAsynce';
 import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const auth = () => {
   return catchAsync(async (req, res, next) => {
@@ -9,6 +11,15 @@ const auth = () => {
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authroized!');
     }
+
+    // check if the token is valid
+    jwt.verify(token, config.jwt_access_secret as string, (err, decoded) => {
+      if (err) {
+        throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authroized!');
+      }
+
+      req.user = decoded as JwtPayload;
+    });
 
     next();
   });
