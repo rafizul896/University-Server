1416d39e528b6b58b0c3d4ba06e290b1703e67a5
rafizul896 <rafizulislam896@gmail.com>
2024-12-27 07:41:21 +0600

    add: auth service and controller file

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import catchAsync from '../../utils/catchAsynce';
+import sendResponse from '../../utils/sendResponse';
+import { AuthServices } from './auth.service';
+import httpStatus from 'http-status';
+
+const loginUser = catchAsync(async (req, res) => {
+  const result = await AuthServices.loginUser(req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User is logged in succesfully!',
+    data: result,
+  });
+});
+
+export const AuthControllers = {
+  loginUser,
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -9,6 +9,7 @@ import { FacultyRoutes } from '../modules/faculty/faculty.route';
 import { CourseRoutes } from '../modules/course/course.route';
 import { SemesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.route';
 import { offeredCourseRoutes } from '../modules/offeredCourse/offeredCourse.route';
+import { AuthRoutes } from '../modules/auth/auth.route';
 
 const router = Router();
 
@@ -52,6 +53,10 @@ const moduleRoutes = [
   {
     path: '/offered-courses',
     route: offeredCourseRoutes,
+  },
+  {
+    path: '/auth',
+    route: AuthRoutes,
   },
 ];
 
